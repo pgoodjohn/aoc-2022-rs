dfe52ec3182101a3cad484e4442f97270f452630
Pietro Bongiovanni <info@pietrobongiovanni.com>
2022-12-01 10:05:01 +0100

    make it more cli-y

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,51 @@
-use plogger;
+use clap::{Parser, Subcommand};
 use log;
+use plogger;
+
+#[derive(Parser)]
+#[clap(about, arg_required_else_help(true))]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Commands>,
+
+    #[clap(short, long)]
+    debug: bool,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    One,
+}
 
 mod one;
 
 fn main() {
-    plogger::init(true);
+    let cli = Cli::parse();
+
+    plogger::init(cli.debug);
 
     log::info!("Welcome to the Advent of Code 2022 Solutions Program");
 
-    match one::run_part_one() {
-        Ok(r) => {
-            log::info!("Day One Part One successful! Result is: {}", r);
-        },
-        Err(e) => {
-            log::error!("Something went wrong: {:?}", e);
-        }
-    }
+    match cli.command {
+        Some(Commands::One) => {
+            match one::run_part_one() {
+                Ok(r) => {
+                    log::info!("Day One Part One successful! Result is: {}", r);
+                }
+                Err(e) => {
+                    log::error!("Something went wrong: {:?}", e);
+                }
+            }
 
-    match one::run_part_two() {
-        Ok(r) => {
-            log::info!("Day One Part Two successful! Result is: {}", r);
+            match one::run_part_two() {
+                Ok(r) => {
+                    log::info!("Day One Part Two successful! Result is: {}", r);
+                }
+                Err(e) => {
+                    log::error!("Something went wrong: {:?}", e);
+                }
+            }
         },
-        Err(e) => {
-            log::error!("Something went wrong: {:?}", e);
-        }
+        None => {} // Handled by Clap
     }
 }
--- src/one/mod.rs
+++ src/one/mod.rs
@@ -2,7 +2,6 @@ use log;
 use std::io::prelude::*;
 
 pub fn run_part_one() -> Result<String, Box<dyn std::error::Error>> {
-
     log::info!("Day One Part One");
 
     let mut all_totals = parse_input()?;
@@ -14,7 +13,7 @@ pub fn run_part_one() -> Result<String, Box<dyn std::error::Error>> {
         Some(r) => {
             log::info!("The Elf carrying the most calories is carrying {}", r);
             Ok(r.to_string())
-        },
+        }
         None => {
             log::error!("Could not get a value from the totals vectors.");
             Err(String::from("Could not get a value from the totals vector").into())
@@ -23,7 +22,6 @@ pub fn run_part_one() -> Result<String, Box<dyn std::error::Error>> {
 }
 
 pub fn run_part_two() -> Result<String, Box<dyn std::error::Error>> {
-
     log::info!("Day One Part Two");
 
     let mut all_totals = parse_input()?;
@@ -33,7 +31,7 @@ pub fn run_part_two() -> Result<String, Box<dyn std::error::Error>> {
     let mut top_three_totals: i32 = 0;
 
     for _ in 0..3 {
-        // Don't want to be bothered with matching here :D 
+        // Don't want to be bothered with matching here :D
         let current_max = all_totals.pop().unwrap();
         top_three_totals = top_three_totals + current_max;
     }
@@ -43,24 +41,26 @@ pub fn run_part_two() -> Result<String, Box<dyn std::error::Error>> {
     Ok(top_three_totals.to_string())
 }
 
-fn parse_input() -> Result<Vec<i32>, Box<dyn::std::error::Error>> {
-
+fn parse_input() -> Result<Vec<i32>, Box<dyn ::std::error::Error>> {
     let input_file = std::fs::File::open("./one/input.txt")?;
     let reader = std::io::BufReader::new(input_file);
 
     let mut current_total: i32 = 0;
 
-    let mut all_totals: Vec<i32> = Vec::new();;
+    let mut all_totals: Vec<i32> = Vec::new();
 
     for line in reader.lines() {
         log::debug!("{:?}", line);
         match line? {
             s if s != String::from("") => {
-                log::info!("Found {}", s);
+                log::debug!("Found {}", s);
                 current_total = current_total + s.parse::<i32>()?;
             }
             _ => {
-                log::info!("Found empty line, adding current total {} to totals vector", current_total);
+                log::debug!(
+                    "Found empty line, adding current total {} to totals vector",
+                    current_total
+                );
                 all_totals.push(current_total);
                 current_total = 0;
             }
